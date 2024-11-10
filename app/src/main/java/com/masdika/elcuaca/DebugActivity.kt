37ac7eb2af5babbe41b0f25afd17a928d5c505b3
Masdikaa <masdikailhanmansiz@gmail.com>
2024-11-10 17:24:01 +0700

    Add testing activity

--- app/src/main/java/com/masdika/elcuaca/DebugActivity.kt
+++ app/src/main/java/com/masdika/elcuaca/DebugActivity.kt
@@ -1,18 +1,11 @@
 package com.masdika.elcuaca
 
-import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 import com.masdika.elcuaca.databinding.ActivityDebugBinding
-import com.masdika.elcuaca.databinding.SnackbarNetworkDisconnectedBinding
 
 class DebugActivity : AppCompatActivity() {
 
@@ -23,12 +16,14 @@ class DebugActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityDebugBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+
     }
+
 
 }
